15ee149e7408d0e0686f3e3c2d7286f3e9d45909
Choi HongCheol <hcchoi@markany.com>
2023-02-13 20:38:32 +0900

    Change grid from div

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import Container from '@mui/material/Container';
+import React from 'react'
+import Container from '@mui/material/Container'
 
-import LoginPage from '@pages/auth/login/LoginPage';
+import LoginPage from '@pages/auth/login/LoginPage'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { grey, yellow } from '@mui/material/colors'
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: yellow[500],
+        light: yellow[400],
+        dark: yellow[600],
+      },
+      secondary: {
+        main: grey[600],
+      },
+    },
+  })
+
   return (
-    <Container maxWidth="md">
-      <LoginPage />
-    </Container>
-  );
+    <ThemeProvider theme={theme}>
+      <Container maxWidth='md'>
+        <LoginPage />
+      </Container>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
--- src/containers/auth/login/LoginContainer.tsx
+++ src/containers/auth/login/LoginContainer.tsx
@@ -1,65 +1,79 @@
-import { AccountCircle, Key } from '@mui/icons-material';
-import { Button, Container, InputAdornment, TextField } from '@mui/material';
-import { grey } from '@mui/material/colors';
-import React from 'react';
+import { AccountCircle, Key } from '@mui/icons-material'
+import { Button, Container, Grid, InputAdornment, Link, TextField, Typography } from '@mui/material'
+import { grey } from '@mui/material/colors'
+import React from 'react'
 
 function LoginContainer() {
   return (
-    <Container
-      className="flex h-screen content-center items-center justify-center"
-      maxWidth="lg"
-    >
-      <div className="basis-full sm:basis-8/12">
-        <div className="font-bol mb-4 text-center text-2xl">AMS</div>
-        <div className="flex basis-full flex-col border-gray-300 sm:basis-5/12 sm:border sm:p-9">
+    <Container maxWidth='lg'>
+      <Grid
+        container
+        direction='column'
+        justifyContent='center'
+        alignItems='center'
+        className='h-screen'
+      >
+        <Grid item className='font-bol mb-4 text-center text-2xl'>
+          AMS
+        </Grid>
+        <Grid item className='border-gray-300 sm:border' p={{ xs: 0, sm: 2 }}>
           <TextField
-            className="mb-6"
-            id="input-with-icon-textfield"
-            label="아이디"
+            fullWidth
+            className='mb-6'
+            id='input-with-icon-textfield'
+            placeholder='아이디를 입력하세요.'
             InputProps={{
               startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle sx={{ color: grey[300] }} />
+                <InputAdornment position='start'>
+                  <AccountCircle color='primary' />
                 </InputAdornment>
               ),
             }}
-            variant="standard"
+            variant='standard'
           />
           <TextField
-            id="input-with-icon-textfield"
-            label="비밀번호"
+            fullWidth
+            id='input-with-icon-textfield'
+            placeholder='비밀번호를 입력하세요.'
             InputProps={{
               startAdornment: (
-                <InputAdornment position="start">
-                  <Key color="success" sx={{ color: grey[300] }} />
+                <InputAdornment position='start'>
+                  <Key color='primary' />
                 </InputAdornment>
               ),
             }}
-            variant="standard"
+            variant='standard'
           />
 
-          <Button
-            fullWidth
-            className="my-7 bg-yellow-300 py-4 text-lg text-black"
-          >
-            로그인
+          <Button fullWidth variant='contained' className='my-5 py-4'>
+            <Typography fontSize='body1'>로그인</Typography>
           </Button>
 
-          <div className="flex justify-between">
-            <div className="hover:cursor-pointer">로그인</div>
-            <div className="">
-              <span className="hover:cursor-pointer">아이디 찾기</span>
-              <span className="mx-4 text-gray-300">|</span>
-              <span className="hover:cursor-pointer">비밀번호찾기</span>
-            </div>
-          </div>
-          <div className="mt-10 text-center text-sm text-gray-400">
+          <Grid container justifyContent='space-between'>
+            <Grid item>
+              <Link href='#' color='secondary' underline='none'>
+                회원가입
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href='#' color='secondary' underline='none'>
+                아이디 찾기
+              </Link>
+              <Typography color='secondary' component='span'>
+                {' | '}
+              </Typography>
+              <Link href='#' color='secondary' underline='none'>
+                비밀번호 찾기
+              </Link>
+            </Grid>
+          </Grid>
+          <Grid className='mt-10 text-center text-sm text-gray-400'>
             Copyright © Neulsang Corp. All rights reserved.
-          </div>
-        </div>
-      </div>
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
-  );
+  )
 }
 
-export default LoginContainer;
+export default LoginContainer
